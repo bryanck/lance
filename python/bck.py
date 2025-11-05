fe291ec1bc00662a0733b204d0856a2bf44f22dc
Bryan Keller <bkeller@netflix.com>
2025-11-05 09:56:04 -0800

    random

--- python/bck.py
+++ python/bck.py
@@ -1,4 +1,24 @@
 from lance_client_bdp import BdpLanceClient
+import time
 
-client=BdpLanceClient()
-print(client.load_dataset("bkeller.foobar").count_rows())
+client = BdpLanceClient()
+ds = client.load_dataset("bkeller.logblob_tsdal_cold_tier_small")
+ds.list_indices()
+ds.prewarm_index("time_series_id_idx")
+
+start = time.time()
+
+scan = ds.scanner(filter="time_series_id='HISETVK74500000000000000000000000003616680'")
+
+end = time.time()
+print(f"time: {end - start}")
+
+reader = scan.to_reader()
+
+end = time.time()
+print(f"time: {end - start}")
+
+reader.read_all()
+
+end = time.time()
+print(f"time: {end - start}")
